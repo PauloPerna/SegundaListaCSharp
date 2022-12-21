70350f9fbb5719257b84a528a26685cd3e6ee800
Paulo Perna <paulo.perna.rocha@gmail.com>
2022-12-21 16:27:00 -0300

    Terceiro Exercício

---
+++ Leitor.cs
@@ -0,0 +1,70 @@
+using CsvHelper;
+using System.Globalization;
+
+namespace Principal
+{
+    public class LeitorCSV
+    {
+        public static int[,] LeitorMatriz(string FullPathMatriz)
+        {
+            // Configuracao do CsvHelper: Não temos um Header
+            var config = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+            };
+
+            // StreamReader, onde passamos o caminho do arquivo que será lido
+            using var reader = new StreamReader(FullPathMatriz);
+            // Criação do CsvParser
+            using var csv = new CsvParser(reader, config);
+
+            // Se não houver registros... retornamos um array vazio
+            if(!csv.Read())
+                return new int[0,0] {};
+
+            // Código semelhante ao feito em aula
+            // Temos como premissa de que a matriz será quadrada
+            var numColunas = csv.Record.Length;
+            var distancias = new int[numColunas, numColunas];
+            // Preenchemos o array distancias com os valores
+            for (int i = 0; i < numColunas; i++)
+            {
+                var linha = csv.Record;
+                for (int j = 0; j < numColunas; j++)
+                {
+                    distancias[i, j] = int.Parse(linha[j]);
+                }
+                csv.Read();
+            }
+
+            return distancias;
+        }
+        public static int[] LeitorPercurso(string FullPathPercurso)
+        {
+            // Configuracao do CsvHelper: Não temos um Header
+            var config = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+            };
+
+            // StreamReader, onde passamos o caminho do arquivo que será lido
+            using var reader = new StreamReader(FullPathPercurso);
+            // Criação do CsvParser
+            using var csv = new CsvParser(reader, config);
+
+            // Se não houver registros... retornamos um array vazio
+            if(!csv.Read())
+                return new int[0] {};
+
+            // Código semelhante ao feito em aula
+            // Temos como premissa de que a matriz será quadrada
+            var numColunas = csv.Record.Length;
+            var percurso = new int[numColunas];
+            // Preenchemos o array percurso com os valores
+            var linha = csv.Record;
+            percurso = Array.ConvertAll(linha, c => int.Parse(c));
+
+            return percurso;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,44 +17,21 @@ namespace Principal
             string distanciasFullName = Path.Combine(desktopPath,distanciasFileName);
             string percursoFullName = Path.Combine(desktopPath,percursoFileName);
 
-            using (var reader = new StreamReader(distanciasFullName))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                var records = csv.GetRecords<Int32>();
-            }
-            
-            // Leitura da matriz de distancias em um array de linhas
-            string[] matriz = System.IO.File.ReadAllLines(distanciasFullName);
-            // Transformamos a matriz de distancias em um jagged array, splitando as linhas
-            string[][] jaggedMatriz = Array.ConvertAll(matriz, c => c.Split(","));
-            // Array onde armazenaremos as distancias lidas
-            // Usamos como premissa de que a matriz sempre será uma matriz quadrada
-            int[,] distancias = new int[matriz.Count(),matriz.Count()];
+            // Chamamos o método static que realiza a leitura da matriz
+            int[,] distancias = LeitorCSV.LeitorMatriz(distanciasFullName);
 
-            // Preenchemos o array distancias
-            for(int i = 0; i<matriz.Count(); i++){
-                for(int j = 0; j<matriz.Count(); j++){
-                    int.TryParse(jaggedMatriz[i][j],out int newItem);
-                    distancias[i,j] = newItem;
-                }
-            }
-
-            // Leitura do percurso em um array
-            string[] percurso = System.IO.File.ReadAllLines(percursoFullName);
-            // Tomamos como premissa de que o percurso está escrito em apenas uma linha!
-            percurso = percurso[0].Split(",");
-            // Armazenamos o percurso como um array de inteiros representando as cidades
-            int[] cidades = Array.ConvertAll(percurso,c => int.Parse(c));
+            // Chamamos o método static que realzia a leitura do percurso
+            int[] percurso = LeitorCSV.LeitorPercurso(percursoFullName);
             
             // Percorremos o array cidades pegando as distancicas das cidades
             //  no array distancias e somando o total na variavel distanciaTotal
             int distanciaTotal = 0;
             int novaDistancia;
-            for(int i = 0; i < cidades.Count()-1; i++)
+            for(int i = 0; i < percurso.Count()-1; i++)
             {
                 // removemos 1 de cidades[i] para obter o índice da cidade no array
                 //  distancias
-                novaDistancia = distancias[cidades[i]-1,cidades[i+1]-1];
+                novaDistancia = distancias[percurso[i]-1,percurso[i+1]-1];
                 distanciaTotal += novaDistancia;
             }
             
