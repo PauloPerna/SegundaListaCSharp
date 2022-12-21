82f674920b6483d692b71dd083d1c7936fb529d1
Paulo Perna <paulo.perna.rocha@gmail.com>
2022-12-21 14:58:01 -0300

    Segundo exercício

--- Program.cs
+++ Program.cs
@@ -6,22 +6,37 @@ namespace Principal
     {
         public static void Main(string[] args)
         {
-            // Distancias conforme exercício do Class
-            int[,] distancias = new int[5,5]
-            {
-                {00,15,30,05,12},
-                {15,00,10,17,28},
-                {30,10,00,03,11},
-                {05,17,03,00,80},
-                {12,28,11,80,00}
-            };
+            // Caminho do Desktop
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            // Nome dos arquivos
+            string distanciasFileName = "matriz.txt";
+            string percursoFileName = "caminho.txt";
+            // Fullname dos arquivos
+            string distanciasFullName = Path.Combine(desktopPath,distanciasFileName);
+            string percursoFullName = Path.Combine(desktopPath,percursoFileName);
 
-            Console.WriteLine("Entre com o percurso no formato \"1, 2, 4, 2\"");
-            // armazenamos as cidades do percurso na variável abaixo
-            string[] input = Console.ReadLine().Split(",");
-            // transformamos em inteiro, assim podemos acessar as distâncias diretamente no
-            //  array distancias
-            int[] cidades = Array.ConvertAll(input, c => int.Parse(c));
+            // Leitura da matriz de distancias em um array de linhas
+            string[] matriz = System.IO.File.ReadAllLines(distanciasFullName);
+            // Transformamos a matriz de distancias em um jagged array, splitando as linhas
+            string[][] jaggedMatriz = Array.ConvertAll(matriz, c => c.Split(","));
+            // Array onde armazenaremos as distancias lidas
+            // Usamos como premissa de que a matriz sempre será uma matriz quadrada
+            int[,] distancias = new int[matriz.Count(),matriz.Count()];
+
+            // Preenchemos o array distancias
+            for(int i = 0; i<matriz.Count(); i++){
+                for(int j = 0; j<matriz.Count(); j++){
+                    int.TryParse(jaggedMatriz[i][j],out int newItem);
+                    distancias[i,j] = newItem;
+                }
+            }
+
+            // Leitura do percurso em um array
+            string[] percurso = System.IO.File.ReadAllLines(percursoFullName);
+            // Tomamos como premissa de que o percurso está escrito em apenas uma linha!
+            percurso = percurso[0].Split(",");
+            // Armazenamos o percurso como um array de inteiros representando as cidades
+            int[] cidades = Array.ConvertAll(percurso,c => int.Parse(c));
             
             // Percorremos o array cidades pegando as distancicas das cidades
             //  no array distancias e somando o total na variavel distanciaTotal
